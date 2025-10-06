2e26c88cfb6b2f0cd7b79f3c8c50bc2ebdf460be
Juliengaspar <julien.gaspar@student.hepl.be>
2025-10-06 15:44:57 +0200

    bulltest-point && text-slidder with dart

--- code/tictic/lib/screens/welcome.dart
+++ code/tictic/lib/screens/welcome.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tictic/constants/colors.dart';
+import 'package:tictic/constants/sizes.dart';
+import 'package:tictic/screens/welcome/widgets/bullet.dart';
+import 'package:tictic/screens/welcome/widgets/bullets.dart';
+import 'package:tictic/screens/welcome/widgets/logo_welcome.dart';
+import 'package:tictic/screens/welcome/widgets/text_slider.dart';
+import 'package:tictic/screens/welcome/widgets/text_slider_with_bullets.dart';
 
-class Welcome extends StatelessWidget {
+import '../../constants/duarations.dart';
+import '../../constants/fonts.dart';
+
+class Welcome extends StatefulWidget {
   const Welcome({super.key});
 
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +33,9 @@ class Welcome extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              Text("Bonjour"),
+              LogoWelcome(),
+              SizedBox(height: kVerticalPadding),
+              TextSliderWithBullets(),
             ],
           ),
         ),
